01046f7992a22febac25a586b5d57df4a59ab444
Jan Maděra <gmaderajan@gmail.com>
2021-03-03 22:18:37 +0100

    test

--- app/src/main/java/cz/maderajan/cryptomock/MainActivity.kt
+++ app/src/main/java/cz/maderajan/cryptomock/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)

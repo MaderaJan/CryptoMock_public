27a113c10904d636088d932fc4b9af24a5c2f816
Jan Maděra <gmaderajan@gmail.com>
2021-03-11 17:52:12 +0100

    improve code and toods

--- app/src/main/java/cz/maderajan/cryptomock/ui/WalletFragment.kt
+++ app/src/main/java/cz/maderajan/cryptomock/ui/WalletFragment.kt
@@ -3,4 +3,10 @@ package cz.maderajan.cryptomock.ui
 class WalletFragment {
 
     // TODO 6. Bindovat view do wallet fragmentu
+//    private lateinit var binding: FragmentWalletBinding
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        binding = FragmentWalletBinding.bind(view)
+//    }
 }
